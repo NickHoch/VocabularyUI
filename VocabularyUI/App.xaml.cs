6a19822b9150131ce6a5a2129f5bcb544f746c29
Nick <kolya.goch@gmail.com>
2018-05-15 18:56:51 +0300

    added method that automatically close child windows after close parent window

--- VocabularyUI/App.xaml.cs
+++ VocabularyUI/App.xaml.cs
@@ -13,5 +13,10 @@ namespace VocabularyClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.ShutdownMode = System.Windows.ShutdownMode.OnMainWindowClose;
+        }
     }
 }
