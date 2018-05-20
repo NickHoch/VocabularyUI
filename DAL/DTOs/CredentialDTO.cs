ff1e58d5b509d9a01f92f9d2e519b50d26350316
Nick <kolya.goch@gmail.com>
2018-05-20 23:45:06 +0300

    app launch in tray and added to app rememberMeChk

--- DAL/DTOs/CredentialDTO.cs
+++ DAL/DTOs/CredentialDTO.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace DAL.DTOs
 {
+    [Serializable]
     public class CredentialDTO
     {
         public string Email { get; set; }
         public string Password { get; set; }
         public ICollection<DictionaryExtnDTO> Dictionaries { get; set; }
+        public CredentialDTO() { }
     }
 }
--- VocabularyUI/MainWindow.xaml.cs
+++ VocabularyUI/MainWindow.xaml.cs
@@ -3,6 +3,11 @@ using System.Windows;
 using VocabularyUI.UserControls;
 using DAL;
 using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO.IsolatedStorage;
+using System.IO;
+using DAL.DTOs;
 
 namespace VocabularyClient
 {
@@ -13,16 +18,63 @@ namespace VocabularyClient
         private Menu menu = null;
         private ServerDAL _dal = new ServerDAL();
         public static Random rand = new Random();
+        private static string projectPath = Directory.GetParent(Directory.GetCurrentDirectory())
+                                         .Parent
+                                         .FullName;
+        private string pathCfg = Path.Combine(projectPath, $@"bin\Debug\credential.txt");
         public MainWindow()
         {
             InitializeComponent();
-            signIn = new SignIn(_dal);
-            contentControl.Content = signIn;
-            signIn.AddHandler(SignIn.SignUpClick, new RoutedEventHandler(SignUpButton));
-            signIn.AddHandler(SignIn.LoginClick, new RoutedEventHandler(LoginButton));
+            var store = IsolatedStorageFile.GetUserStoreForAssembly();
+            BinaryFormatter formatter = new BinaryFormatter();
+            CredentialDTO credential = new CredentialDTO();
+            if(File.Exists("credential.txt"))
+            {
+                using (var stream = store.OpenFile("credential.txt", FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    credential = (CredentialDTO)formatter.Deserialize(stream);
+                }
+                var userId = _dal.GetUserIdByCredential(credential);
+                if (userId.HasValue)
+                {
+                    menu = new Menu(_dal, (int)signIn.userId);
+                    contentControl.Content = menu;
+                    menu.AddHandler(Menu.ExitClick, new RoutedEventHandler(ExitButton));
+                    menu.AddHandler(Menu.LogOutClick, new RoutedEventHandler(LogOutButton));
+                }
+                //else
+                //{
+                //    signIn = new SignIn(_dal);
+                //    contentControl.Content = signIn;
+                //    signIn.AddHandler(SignIn.SignUpClick, new RoutedEventHandler(SignUpButton));
+                //    signIn.AddHandler(SignIn.LoginClick, new RoutedEventHandler(LoginButton));
+                //}
+            }
+            else
+            {
+                signIn = new SignIn(_dal);
+                contentControl.Content = signIn;
+                signIn.AddHandler(SignIn.SignUpClick, new RoutedEventHandler(SignUpButton));
+                signIn.AddHandler(SignIn.LoginClick, new RoutedEventHandler(LoginButton));
+            }
+          
         }
         private void LoginButton(object sender, RoutedEventArgs e)
         {
+            if(signIn.rememberChk.IsChecked == true)
+            {
+                CredentialDTO credential = new CredentialDTO
+                {
+                    Email = signIn.loginField.Text,
+                    Password = signIn.passwordField.Password
+                };
+                BinaryFormatter formatter = new BinaryFormatter();
+                var store = IsolatedStorageFile.GetUserStoreForAssembly();
+                using (var stream = store.OpenFile("credential.txt", FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    formatter.Serialize(stream, credential);
+                }
+            }
             menu = new Menu(_dal, (int)signIn.userId);
             contentControl.Content = menu;
             menu.AddHandler(Menu.ExitClick, new RoutedEventHandler(ExitButton));
