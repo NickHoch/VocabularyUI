6468b4685770d2607d7d7dc911b5f6e37e7c8d8c
Sergey Korolchuk <antinomy.rv@gmail.com>
2018-07-04 10:21:24 +0300

    start unit test dal

---
+++ TestClient/TestDal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DAL;
+using DAL.ServiceVocabulary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestClient
+{
+    [TestClass]
+    public class TestDal
+    {
+        ServerDAL serverDAL;
+        Mock<VocabularyClient> mock;
+        [TestInitialize]
+        public void Init()
+        {
+            serverDAL = new ServerDAL();
+            mock = new Mock<VocabularyClient>();
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            serverDAL = null;
+            mock = null;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
